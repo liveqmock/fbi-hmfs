92ef956b4c7d7e64a5da9c248b952a48b6d94de3
zhanrui@gmail.com <zhanrui@gmail.com@7fc9343e-2403-c20f-2291-c02dad1d7819>
2012-03-29 04:13:47 +0000

    对帐

--- hmfs-1.0/common/java/common/enums/SysCtlSts.java
+++ hmfs-1.0/common/java/common/enums/SysCtlSts.java
@@ -10,12 +10,12 @@ public enum SysCtlSts implements EnumApp {
     INIT("0", "系统初始化"),
     SIGNON("1", "已签到"),
     SIGNOUT("2", "已签退"),
-    HOST_BALCHK_SUCCESS("3", "主机余额对账成功"),
-    HOST_DETLCHK_SUCCESS("4", "主机流水对账成功"),
-    HOST_CHK_SUCCESS("5", "主机对账成功"),
-    HMB_BALCHK_SUCCESS("6", "国土局余额对账成功"),
-    HMB_DETLCHK_SUCCESS("7", "国土局流水对账成功"),
-    HMB_CHK_SUCCESS("8", "国土局对账成功");
+    //HOST_BALCHK_SUCCESS("3", "主机余额对账成功"),
+    //HOST_DETLCHK_SUCCESS("4", "主机流水对账成功"),
+    HOST_CHK_SUCCESS("3", "主机对账成功"),
+    //HMB_BALCHK_SUCCESS("6", "国土局余额对账成功"),
+    //HMB_DETLCHK_SUCCESS("7", "国土局流水对账成功"),
+    HMB_CHK_SUCCESS("4", "国土局对账成功");
 
     private String code = null;
     private String title = null;
