b3f735e82c1e2f10876e471fbb2039f6ef04c75b
zhanrui@gmail.com <zhanrui@gmail.com@7fc9343e-2403-c20f-2291-c02dad1d7819>
2012-07-04 08:17:30 +0000

    数据移植 核对

---
+++ hmfs-1.0/web/java/hmfs/datamigration/DataMigration.groovy
@@ -0,0 +1,85 @@
+package hmfs.datamigration
+
+import groovy.sql.Sql
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhanrui
+ * Date: 12-7-2
+ * Time: 下午4:27
+ * To change this template use File | Settings | File Templates.
+ */
+class DataMigration {
+    def localdbdef = [url: 'jdbc:oracle:thin:@localhost:1521:orcl', user: 'hmfs', password: 'hmfs', driver: 'oracle.jdbc.driver.OracleDriver']
+    def localdb = Sql.newInstance(localdbdef.url, localdbdef.user, localdbdef.password, localdbdef.driver)
+
+    static void main(args) {
+        DataMigration mig = new DataMigration();
+        mig.checkFundAct()
+        mig.checkFundTxn()
+    }
+
+    def checkFundAct() {
+        localdb.eachRow("select * from mig_acct where parent_fund is null") {
+            def fund = it.fund
+            def bal = it.bal
+            def sumbal = 0;
+            def count = 0;
+            localdb.eachRow("select count(*) as cnt,sum(bal) as bal from mig_acct where parent_fund = ${fund}") {
+                sumbal = it.bal
+                count = it.cnt
+            }
+            if (sumbal == null) {
+                sumbal = 0
+                count = 0
+            }
+            if (bal != sumbal) {
+                println "======核算户:${fund} 余额:${bal}, 分户余额合计:${sumbal}, 分户数:${count} "
+            }
+
+            /*
+            localdb.eachRow("select * from mig_acct where parent_fund = ${fund}") {
+                localdb.eachRow("select count(*) cnt from mig_acct where parent_fund = ${it.fund}") {
+                    if (it.cnt > 0) {
+                        println "存在三级核算户"
+                    }
+                }
+            }
+            */
+        }
+    }
+
+
+    def checkFundTxn() {
+        localdb.eachRow("select * from mig_acct") {
+            def fund = it.fund
+            def bal = it.bal
+            def txnamt = 0
+
+            if (bal != 0) {
+                def sql = """
+                        select sum(case
+                                     when (t.is_cancel = '0' and t.tran_flag = '2') then
+                                      t.tran_amt
+                                     when (t.is_cancel = '0' and t.tran_flag = '1') then
+                                      -t.tran_amt
+                                     when (t.is_cancel = '1' and t.tran_flag = '2') then
+                                      -t.tran_amt
+                                     when (t.is_cancel = '1' and t.tran_flag = '1') then
+                                      t.tran_amt
+                                   end) as amt
+                          from MIG_ACCT_WATER t
+                         where t.fund = ${fund}
+                    """
+                localdb.eachRow(sql) {
+                    txnamt = it.amt
+                }
+                if (bal != txnamt) {
+                    println "======核算户:${fund} 余额:${bal}, 流水表发生额合计:${txnamt} "
+                }
+            }
+        }
+    }
+
+
+}
