67d5df386589daa3439730430ae0a6a59b1ccca1
hanjianlong@rongshangsoft.com <hanjianlong@rongshangsoft.com@7fc9343e-2403-c20f-2291-c02dad1d7819>
2013-01-08 01:35:18 +0000

    Failed commit: Default

--- hmfs-1.0/dep/java/dep/hmfs/online/processor/cbs/CbsTxn6001Processor.java
+++ hmfs-1.0/dep/java/dep/hmfs/online/processor/cbs/CbsTxn6001Processor.java
@@ -35,18 +35,26 @@ public class CbsTxn6001Processor extends CbsAbstractTxnProcessor {
         TOA6001 toa6001 = new TOA6001();
         // 查询交款汇总信息
         HmActFund actFund = hmbActinfoService.qryActfundByActNo(tia6001.body.fundActno);
-
+        // 业主姓名
         toa6001.body.accountName = actFund.getInfoName();
-        toa6001.body.txAmt = String.format("%.2f", actFund.getHouseTotalAmt());
-        toa6001.body.address = actFund.getInfoAddr();    //22
+        // 交易金额
+        //toa6001.body.txAmt = String.format("%.2f", actFund.getHouseTotalAmt()); // hanjianlong 20121226 del
+        toa6001.body.txAmt = String.format("%.2f", actFund.getActBal()); // hanjianlong 20121226 add
+        // 住宅地址
+        toa6001.body.address = actFund.getInfoAddr();
+        // 住宅建筑面积
         toa6001.body.houseArea = StringUtils.isEmpty(actFund.getBuilderArea()) ? "" : actFund.getBuilderArea();
-
+        // 住宅类型
         toa6001.body.houseType = actFund.getHouseDepType();
+        // 联系电话
         toa6001.body.phoneNo = actFund.getHouseCustPhone();
+        // 交存表标准2
         String field83 = actFund.getDepStandard2();
+        // 交存表标准2为空，则工程造价和交存比例都为空
         if (field83 == null) {
             toa6001.body.projAmt = "";
             toa6001.body.payPart = "";
+            // 交存表标准2中的"|"前卫工程造价，后为交存比例
         } else if (field83.endsWith("|") || !field83.contains("|")) {
             toa6001.body.projAmt = new StringBuilder(field83).deleteCharAt(field83.length() - 1).toString();
             toa6001.body.payPart = "";
@@ -55,7 +63,8 @@ public class CbsTxn6001Processor extends CbsAbstractTxnProcessor {
             toa6001.body.projAmt = fields83[0];
             toa6001.body.payPart = fields83[1];
         }
-        toa6001.body.accountNo = actFund.getFundActno1();  // 业主核算户账号(维修资金账号)
+        // 业主核算户账号(维修资金账号)
+        toa6001.body.accountNo = actFund.getFundActno1();
         return toa6001;
     }
 }
